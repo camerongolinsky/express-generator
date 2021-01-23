501279fae5a8e11d95ae040da7a82632d2d50025
Cameron Golinsky <camerongolinsky@gmail.com>
2021-01-23 01:24:33 -0800

    facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey':'12345-12345-12345-12345',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '316225819822800',
+        clientSecret: 'd8806c2259f6a957373fc560e80050c4'
+    }
 }
